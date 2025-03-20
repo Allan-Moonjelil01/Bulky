d1e56f1f723cd60b535796864fd3131c7072be36
Allan Moonjelil <allan.moonjelil01@whitireianz.ac.nz>
2025-03-21 11:42:26 +1300

    Added updation & deletion of category functionality.Also added toastr library to show the create,update & delete messages.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,6 +17,7 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
+        //CREATE BUTTON
         public IActionResult Create()
         {
             return View();
@@ -25,9 +26,96 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                TempData["error"] = "Error";
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match with the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //EDIT BUTTON
+        public IActionResult Edit(int id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+
+        //EDIT BUTTON
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            return View(categoryFromDb);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted succesfully";
             return RedirectToAction("Index");
         }
+
+
+
+
+        public string GetAllCategories()
+        {
+            return "Return All Categories";
+        }
+
+        public string GetCategoriesByName(string name)
+        {
+            return $"Return All Categories :{name}";
+        }
+
+
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -9,9 +9,11 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(20)]
         [DisplayName("Category Name")]
         public String Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Please enter between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
